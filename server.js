9ce9ff55bac11105682752067a9cef6abfdd638d
Jimmie Harkins <ghana796@gmail.com>
2021-04-21 00:16:26 -0400

    First commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
+const unirest = require("unirest");
+const API_KEY = "112a5230-4cfe-4848-9048-9f07c11b94d4";
+
+app.get('/api/associations/:word', (req, res) => {
+	const word = req.params.word;
+	const request = unirest.get(`https://api.wordassociations.net/associations/v1.0/json/search?apikey=${API_KEY}&lang=en&text=${word}`)
+	.then(response => {
+		const results = response.body.response[0].items || []; // grab array of results
+		console.log(`Num results=${results.length}`);
+		res.json(results);
+	})
+	.catch(error => {
+		console.log(`error=${error}`);
+		res.json({status:"Error", message: `${error}`});
+	});
+});
+
+app.listen(port, () => {
+  console.log(`word-app listening on port ${port}`);
+});
